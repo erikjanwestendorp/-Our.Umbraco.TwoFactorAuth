d2504f4c8aee2d2f58aaabdebe6bbc9838bedb09
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2022-08-29 15:14:36 +0200

    Move files to assets projects

---
+++ src/Our.Umbraco.TwoFactorAuth.Assets/ManifestFilter.cs
@@ -0,0 +1,19 @@
+﻿using Umbraco.Cms.Core.Manifest;
+
+namespace Our.Umbraco.TwoFactorAuth.Assets;
+
+internal class ManifestFilter : IManifestFilter
+{
+    public void Filter(List<PackageManifest> manifests)
+    {
+        manifests.Add(new PackageManifest
+        {
+            PackageName = "Our.Umbraco.TwoFactorAuth",
+            Scripts = new[]
+            {
+                "~/App_Plugins/Our.Umbraco.TwoFactorAuth/two-factor-authentication.controller.js"
+            }, 
+            Version = "1.0.0"
+        });
+    }
+}
